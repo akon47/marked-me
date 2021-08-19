95ec917a738a939c0abe569b9377eee5a6eb53a5
Kim Hwan <akon47@naver.com>
2021-08-19 14:19:07 +0900

    헤더부분 스타일링, 푸터 사이즈 줄임

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -1,9 +1,43 @@
 <template>
-  <div></div>
+  <div class="header-background">
+    <div class="title">kimhwan.kr</div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.header-background {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(10deg, #a1d9b4 20%, #32988a 30%, #000e3c 70%);
+  background-repeat: no-repeat;
+  background-size: 1000% 100%;
+  animation: Gradient 60s ease infinite;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+@keyframes Gradient {
+  0% {
+    background-position: 40% 50%;
+  }
+  50% {
+    background-position: 70% 50%;
+  }
+  100% {
+    background-position: 40% 50%;
+  }
+}
+
+.header-background .title {
+  color: white;
+  font-weight: bold;
+  font-size: 40pt;
+}
+</style>
