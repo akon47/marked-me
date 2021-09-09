18e1a7fa2fc2c88f085949175c034a901207c074
Kim Hwan <akon47@naver.com>
2021-09-09 12:50:03 +0900

    feat: Modified to replace e-mail address with <a> tag

--- src/api/markdown-parser.ts
+++ src/api/markdown-parser.ts
@@ -13,6 +13,10 @@ const markedOptions = {
   xhtml: true,
 } as marked.MarkedOptions;
 
+const emailRegex =
+  /(([^<>()[\]\\.,;:\s@\\"]+(\.[^<>()[\]\\.,;:\s@\\"]+)*)|(\\".+\\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
+
 export function toHtml(markdownText: string): string {
-  return marked(markdownText, markedOptions);
+  const html = marked(markdownText, markedOptions);
+  return html.replace(emailRegex, '<a href="mailto:$&">$&</a>');
 }
